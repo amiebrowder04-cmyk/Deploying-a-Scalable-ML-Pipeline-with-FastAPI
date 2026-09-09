f00ee2c953414303ea609d281cdc554944d41d15
abrowd1 <abrowd1@wgu.edu>
2026-09-09 12:43:50 -0700

    Implement model saving

--- ml/model.py
+++ ml/model.py
@@ -78,8 +78,8 @@ def save_model(model, path):
     path : str
         Path to save pickle file.
     """
-    # TODO: implement the function
-    pass
+    with open(path, "wb") as file:
+        pickle.dump(model, file)
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
